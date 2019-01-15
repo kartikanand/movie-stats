2ced12937dc4ad2b4d390c75c2d648f02d377560
kartik <kartikanand1992@gmail.com>
2019-01-15 04:12:40 -0600

    add api request

--- parser.py
+++ parser.py
@@ -1,11 +1,27 @@
 import csv
+import os
+import re
+
 import requests
+
+OMDB_API_URL = 'https://www.omdbapi.com'
+pattern = re.compile('([^\s\w]|_)+')
 
 with open('watched.csv') as f:
     mreader = csv.DictReader(f)
 
     for row in mreader:
-        name = row['Name']
+        # strip characters other than alpha-numeric
+        # for some movies this has a better chance of a direct hit
+        name = pattern.sub('', row['Name'])
         year = row['Year']
-        break
+
+        payload = {
+            't': name,
+            'y': year,
+            'apikey': os.getenv('OMDB_API_KEY', '')
+        }
+
+        r = requests.get(OMDB_API_URL, params=payload)
+
 
