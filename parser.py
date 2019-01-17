81c1e932f37a5e2410eea9cec57c7f4a6f08576f
kartik <kartikanand1992@gmail.com>
2019-01-17 02:55:30 -0600

    call movie stats while parsing movies list

--- parser.py
+++ parser.py
@@ -4,8 +4,12 @@ import re
 
 import requests
 
+from moviestats import MovieStats
+
 OMDB_API_URL = 'https://www.omdbapi.com'
 pattern = re.compile('([^\s\w]|_)+')
+
+ms = MovieStats()
 
 with open('watched.csv') as f:
     mreader = csv.DictReader(f)
@@ -24,4 +28,13 @@ with open('watched.csv') as f:
 
         r = requests.get(OMDB_API_URL, params=payload)
 
+        # get json object from response object
+        # this is then passed to a movie stats object
+        movie_json = r.json()
+
+        # add movie to movie stats object which will process
+        # it internally
+        ms.add_movie(movie_json)
+
+ms.print_stats()
 
