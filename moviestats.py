5d6abfb38684c93989cd8150c66ea46743d2682d
kartik <kartikanand1992@gmail.com>
2019-01-17 02:54:46 -0600

    fix syntax errors

--- moviestats.py
+++ moviestats.py
@@ -40,46 +40,46 @@ class MovieStats:
         director_lst = movie['Director'].strip().split(', ')
         self.add_directors(director_lst)
 
-    def add_genre(genre):
+    def add_genre(self, genre):
         self.genre_count[genre] += 1
 
-    def add_genres(genre_lst):
+    def add_genres(self, genre_lst):
         for genre in genre_lst:
             self.add_genre(genre)
 
-    def add_actor(actor):
+    def add_actor(self, actor):
         self.actor_count[actor] += 1
 
-    def add_actors(actor_lst):
-        for actor in actor_lst
+    def add_actors(self, actor_lst):
+        for actor in actor_lst:
             self.add_actor(actor)
 
-    def add_director(director):
+    def add_director(self, director):
         self.director_count[director] += 1
 
-    def add_directors(director_lst):
+    def add_directors(self, director_lst):
         for director in director_lst:
             self.add_director(director)
 
-    def get_max_count_genres():
+    def get_max_count_genres(self):
         return get_max_count_items(self.genre_count)
 
-    def get_max_count_actors():
+    def get_max_count_actors(self):
         return get_max_count_items(self.actor_count)
 
-    def get_max_count_directors():
+    def get_max_count_directors(self):
         return get_max_count_items(self.director_count)
 
-    def print_stats():
+    def print_stats(self):
         # get genres with highest count
         print('Most loved genres')
-        print(self.get_max_count_genres)
+        print(self.get_max_count_genres())
 
         # get actors with highest count
         print('Most loved actors')
-        print(self.get_max_count_actors)
+        print(self.get_max_count_actors())
 
         # get directors with highest count
         print('Most loved directors')
-        print(self.get_max_count_directors)
+        print(self.get_max_count_directors())
 
